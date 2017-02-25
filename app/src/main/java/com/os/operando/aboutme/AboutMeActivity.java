22e417292a0b3195457aecdf7508e0647f8f847a
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2017-02-25 16:34:15 +0900

    Add Alerter
    https://github.com/Tapadoo/Alerter

--- app/src/main/java/com/os/operando/aboutme/AboutMeActivity.java
+++ app/src/main/java/com/os/operando/aboutme/AboutMeActivity.java
@@ -1,13 +1,26 @@
 package com.os.operando.aboutme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.os.operando.aboutme.databinding.ActivityAboutMeBinding;
+import com.tapadoo.alerter.Alerter;
 
 public class AboutMeActivity extends AppCompatActivity {
+
+    private ActivityAboutMeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_me);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_about_me);
+
+        binding.push.setOnClickListener(v -> {
+            Alerter.create(this)
+                    .setTitle(R.string.AboutMeActivity_push)
+                    .setText(R.string.AboutMeActivity_push)
+                    .show();
+        });
     }
 }
